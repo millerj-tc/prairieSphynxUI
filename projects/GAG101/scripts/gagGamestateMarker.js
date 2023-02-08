ddbd4106cc522a419ed7b2f3ef903e65bd1526c8
millerj-tc <j.sam.miller@gmail.com>
2023-02-08 00:14:49 -0600

    gamestateMarker

--- projects/GAG101/scripts/gagGamestateMarker.js
+++ projects/GAG101/scripts/gagGamestateMarker.js
@@ -4,5 +4,21 @@ export class gagGamestateMarker
         
         this.runId = runId;
         this.funcName = funcName;
+        this.time = Date.now();
+    }
+    
+    static GetObjFromArrByGamestateFuncName(arr,funcName){
+        
+        for(const o of arr){
+            
+            if(o.hasOwnProperty("gamestateMarker") && o.gamestateMarker.funcName == funcName) return o
+        }
+        
+        //>>> does below work?
+        
+//        for(const prop in o){
+//            
+//            if(prop.isPrototypeOf("gagGamestateMarker"))
+//        }
     }
 }
