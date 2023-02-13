99fcc8a10d1979ccbd5401b9a0f97e09f347e136
millerj-tc <j.sam.miller@gmail.com>
2023-02-12 20:59:38 -0600

    outputTextDiv

--- projects/GAG101/scripts/scenario/scenarioPhases/outputUtils.js
+++ projects/GAG101/scripts/scenario/scenarioPhases/outputUtils.js
@@ -1,4 +1,15 @@
-export function OutputTextWithCharacterImages(charArr,noTeamArr,noNameArr){
+export function OutputTextDivWithNounImages(string){
     
+    //what if it could take any number of arguments (including 0) and did different things if they were arrays or single objects? That way you could include different groups, etc.
     
+    //Also build in all the steps of dupeCunk output (replace based on plural subjects, specify pronouns, etc.)
+    
+    //"[argNN[name (for image)]team?]: Welcome to the Dance of Riddles, [arg01[(use GetSpanList)/team/name]]
+    
+    const nounGroupsArr = arguments.slice(1);
+    
+    for(const m of string.match(/\[arg**\[(.*?)\]\]/)){
+        
+        console.log(m);
+    }
 }
