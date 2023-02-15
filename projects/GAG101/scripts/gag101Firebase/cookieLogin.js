66fe28b5bb94f1ed9447a716d1c1a36a67938319
jmillerLEARTH <99913522+jmillerLEARTH@users.noreply.github.com>
2023-02-15 16:23:48 -0600

    cookie login !

--- projects/GAG101/scripts/gag101Firebase/cookieLogin.js
+++ projects/GAG101/scripts/gag101Firebase/cookieLogin.js
@@ -10,10 +10,60 @@ export function LoginWithCookie(){
     
     const db = getDatabase();
     
+    _GetCookie();
+    
      const poems = ref(db, 'characters/berin/heardPoems');
     onValue(poems, (snapshot) => {
         const data = snapshot.val();
 
         console.log(data);
     });
+}
+
+function _GetCookie(){
+    
+    let uid = localStorage.getItem("gag101FirebaseKey");//null//
+    
+     const db = getDatabase();
+    
+    if(uid == null){
+        uid = (Number(Date.now()) + (Math.random()*1000)).toFixed(0);
+        localStorage.setItem("gag101FirebaseKey",uid);
+        _AddUIDToRegistry(uid);
+        
+    }
+    
+    const users = ref(db, 'GAG101Users');
+     onValue(users, (snapshot) => {
+        const data = snapshot.val();
+
+        console.log(data);
+    });
+    
+    
+    
+    
+    
+    console.log(uid);
+        
+    return uid
+}
+
+function _AddUIDToRegistry(uid){
+    
+    const db = getDatabase();
+    
+    const postData = {
+    uid: uid
+
+  };
+
+  // Get a key for a new Post.
+  const newUidKey = push(child(ref(db), 'GAG101Users ')).key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  const updates = {};
+  updates['/GAG101Users/' + newUidKey + `/` ] = postData;
+
+  return update(ref(db), updates);
 }
