4fe099b2e2969a4af3f0e12b4246c2e5d5fff3c6
millerj-tc <j.sam.miller@gmail.com>
2023-02-22 21:39:14 -0600

    notes adjustment

--- projects/GAG101/scripts/pvp/pvpScenarioTournament.js
+++ projects/GAG101/scripts/pvp/pvpScenarioTournament.js
@@ -1,15 +1,7 @@
-// load existing submission, if any
 // display leaderboard
 // allow player to choose from their collection
 // warn if submission within the last 24 hours and do not submit (get from user profile, not from gag101scenarios)
-// retrieve top 5 submissions
-// run each match, pausing between w/"continue" button to keep going
-// if new deck did better than any existing deck, adjust win rates
-// some kind of overall tournament result message
-// adjust leaderboard
 // give user timestamp of submission for that scenario
-// if user has existing deck in top 10, replace that deck, otherwise add it
-// then, if there are 11 decks in the top 10, remove the lowest ranked one (ties broken by earliest submission)
 
 //firebase scenario structure:
 //gag101scenarios
